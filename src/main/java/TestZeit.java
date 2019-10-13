a0af357b76434c41b7148ecb14714f6294d02378
Tim <tim.lehmkuhl@stud.th-luebeck.de>
2019-10-13 19:14:50 +0200

    ST Zeit

--- src/main/java/TestZeit.java
+++ src/main/java/TestZeit.java
@@ -4,8 +4,9 @@ import org.stringtemplate.v4.*;
 
 public class TestZeit {
     public static void main(String[] args) throws Exception {
-        //STGroup templates = new STGroupFile("…");
-      //  ST st = templates.getInstanceOf("…");
+        STGroup templates = new STGroupFile("G:\\InfProjekte\\LexikalischeFilter\\src\\main\\java\\zeit.stg");
+
+      //  System.out.println( "Name: " + st.getName());
         CharStream input = null;
         // Pick an input stream (filename from commandline or stdin)
         if (args.length>0) input = CharStreams.fromFileName(args[0]);
@@ -14,12 +15,23 @@ public class TestZeit {
         Token t=null;
         do {
             t = lex.nextToken();
-            if(t.getType() != Zeit.EOF) {
-	System.out.println(t.getText() + " Valid: " + isValidTime(t.getText()));
+            if(t.getType() == Zeit.TIME && isValidTime(t.getText())) {
+	           // System.out.println(t.getText() + " Valid: " + isValidTime(t.getText()));
+              /*  st.add("stunden", t.getText().substring(0,2));
+	            st.add("minuten", t.getText().substring(3,5));
+                st.add("sekunden", t.getText().substring(6,8));
+                st.add("doppelpunkt", t.getText().charAt(2));*/
+
+                ST st = templates.getInstanceOf("zeit");
+                st.add("zeiteinheit", t.getText());
+
+                System.out.println(st.render());
 
             }
         } while ( t.getType()!=Token.EOF );
     }
+
+
 
     public static boolean isValidTime(String time){
         int stunden = Integer.parseInt(time.substring(0,2));
@@ -27,7 +39,10 @@ public class TestZeit {
         int sekunden = 0;
         if(time.length() > 5) sekunden = Integer.parseInt(time.substring(6,8));
 
-        if((stunden > 23 && stunden >=0) | (minuten > 59 && minuten >=0) | (sekunden > 59 && sekunden >=0)) return false;
+        if((stunden > 23 && stunden >=0) | (minuten > 59 && minuten >=0) | (sekunden > 59 && sekunden >=0)){
+            System.err.println(time + " ist Keine gueltige Uhrzeit!");
+            return false;
+        }
 
         return true;
     }
